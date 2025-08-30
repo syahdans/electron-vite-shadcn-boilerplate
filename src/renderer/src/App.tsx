af63a6511adb8cf05650f4aa7cecfe8bfe67ff79
syahdans <syahdan@alumni.pcr.ac.id>
2025-08-30 14:07:01 +0700

    default button shadcn --checkpoint

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,34 +1,15 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { Button } from '@renderer/components/ui/button'
 
 function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p className="bg-rose-200 p-2 m-2 rounded">
+        Dolor cillum deserunt culpa in cillum enim culpa veniam.
       </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
+      <Button variant="default" size="lg">
+        ShadCN Button
+      </Button>
+    </div>
   )
 }
 
